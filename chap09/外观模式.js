2c61d0c1d9f7306fc5f9fd2cc0d09a08b009b2df
BendMoly <ssomkhyy@qq.com>
2017-03-23 00:06:40 +0800

    结构型设计模式
    外观模式与适配器模式

---
+++ chap09/外观模式.js
@@ -0,0 +1,37 @@
+// 外观模式实现
+function addEvent(dom, type, fn){
+	// 对于支持DOM2级事件处理程序addEventListener方法的浏览器
+	if (dom.addEventListener) {
+		dom.addEventListener(type, fn, false);
+	}else if (dom.attachEvent) {
+		//对于不支持addEventListener方法但支持attachEvent方法的浏览器
+		dom.attachEvent('on' + type, fn);
+	}else{
+		// 对于不支持addEventListener方法，也不支持attachEvent方法
+		// 但支持on + '事件名'的浏览器
+		dom['on' + type] = fn;
+	}
+}
+
+// 获取事件对象
+var getEvent = function(event){
+	// 标准浏览器返回event，IE下window.event
+	return event || window.event;
+}
+// 获取元素
+var getTarget = function(event){
+	var evt = getEvent(event);
+	// 标准浏览器下event.target, IE下event.srcElement
+	return event.target || event.srcElement;
+}
+// 阻止默认行为
+var preventDefault = function(event){
+	var evt = getEvent(event);
+	// 标准浏览器
+	if (event.preventDefault) {
+		event.preventDefault();
+	}else{
+		//IE浏览器
+		event.returnValue = false;
+	}
+}
---
+++ chap10/适配器模式.js
@@ -0,0 +1,48 @@
+// 如果两个框架代码的书写方式相似
+window.A = A = JQuery;	//以JQ举例
+
+// 如果两个框架代码的书写方式相差太远
+// 定义框架
+var A = A || {};
+// 获取id元素
+A.g = function(id){
+	return document.getElementById(id);
+}
+// 为元素绑定事件
+A.on = function(id, type, fn){
+	// 如果传递参数是字符串则以id处理，否则以元素对象处理
+	var dom = typeof id === 'string' ? this.g(id) : id;
+	// 对于支持DOM2级事件处理程序addEventListener方法的浏览器
+	if (dom.addEventListener) {
+		dom.addEventListener(type, fn, false);
+	}else if (dom.attachEvent) {
+		//对于不支持addEventListener方法但支持attachEvent方法的浏览器
+		dom.attachEvent('on' + type, fn);
+	}else{
+		// 对于不支持addEventListener方法，也不支持attachEvent方法
+		// 但支持on + '事件名'的浏览器
+		dom['on' + type] = fn;
+	}
+}
+// =============> 以A框架与JQuery适配为例
+A.g = function(id){
+	// 通过JQ获取对象，返回第一个成员
+	return $(id).get(0);
+}
+A.on = function(id, type, fn){
+	// 如果传递参数是字符串则以id处理，否则以元素对象处理
+	var dom = typeof id === 'string' ? $('#' + id) : $(id);
+	dom.on(type, fn);
+}
+
+// 适配参数
+function doSomeThing(obj){
+	var _default = {
+		name: ''
+		//...
+	};
+	for(var i in _default){
+		_default[i] = obj[i] || _default[i]
+	}
+	//如果obj有其他属性是默认属性没有的，根据需求进行扩展
+}
